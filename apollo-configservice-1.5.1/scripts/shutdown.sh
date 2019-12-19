9ab5c1e4c3af80db680b7fcd8295790616b0a8be
梁润星 <runxing.liang@poi-t.com>
2019-12-19 15:19:27 +0800

    'test'

---
+++ apollo-configservice-1.5.1/scripts/shutdown.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+SERVICE_NAME=apollo-configservice
+
+if [[ -z "$JAVA_HOME" && -d /usr/java/latest/ ]]; then
+    export JAVA_HOME=/usr/java/latest/
+fi
+
+cd `dirname $0`/..
+
+if [[ ! -f $SERVICE_NAME".jar" && -d current ]]; then
+    cd current
+fi
+
+if [[ -f $SERVICE_NAME".jar" ]]; then
+  chmod a+x $SERVICE_NAME".jar"
+  ./$SERVICE_NAME".jar" stop
+fi
+export JAVA_OPTS="-server -Xms6144m -Xmx6144m -Xss256k -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=384m -XX:NewSize=4096m -XX:MaxNewSize=4096m -XX:SurvivorRatio=18"
